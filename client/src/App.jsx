174ac815d978c1eb598a49f87c004705c0430259
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2023-08-19 16:34:29 +0500

    minor updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,12 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
-import { SocketManager } from "./components/SocketManager";
 
 function App() {
   return (
-    <>
-      <SocketManager />
-      <Canvas shadows camera={{ position: [8, 8, 8], fov: 30 }}>
-        <color attach="background" args={["#ececec"]} />
-        <Experience />
-      </Canvas>
-    </>
+    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+      <color attach="background" args={["#ececec"]} />
+      <Experience />
+    </Canvas>
   );
 }
 
