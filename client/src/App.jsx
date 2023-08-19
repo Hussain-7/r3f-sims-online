c612fcb4bcfe3302fec78b541c9bad0165c63230
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2023-08-20 00:30:01 +0500

    new characters added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { Experience } from "./components/Experience";
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+    <Canvas shadows camera={{ position: [5, 5, 10], fov: 30 }}>
       <color attach="background" args={["#ececec"]} />
       <Experience />
     </Canvas>
--- client/src/components/Experience.jsx
+++ client/src/components/Experience.jsx
@@ -1,13 +1,20 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls } from "@react-three/drei";
+import { Adventurer } from "./Characters/Adventurer";
+import { Witch } from "./Characters/Witch";
+import { BeachCharacter } from "./Characters/BeachCharacter";
+import { ScifiCharacter } from "./Characters/ScifiCharacter";
+import { HoodedAdventurer } from "./Characters/HoodedAdventurer";
 
 export const Experience = () => {
   return (
     <>
+      <Environment preset="sunset" background />
+      <ambientLight intensity={0.5} />
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <BeachCharacter position={[0, 0, 0]} hairColor="red" />
+      <BeachCharacter position={[1, 0, 0]} />
+      <BeachCharacter position={[2, 0, 0]} />
+      <BeachCharacter position={[3, 0, 0]} />
     </>
   );
 };
