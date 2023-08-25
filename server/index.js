43491f6a15a97b30b46d350f883c4e2c5c0faf71
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2023-08-26 01:56:49 +0500

    path finding added to node js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import { Server } from "socket.io";
 import { map, items } from "./config.js";
+import pathfinding from "pathfinding";
 const io = new Server({
   cors: {
     origin: "http://127.0.0.1:5173",
@@ -8,6 +9,45 @@ const io = new Server({
 });
 
 io.listen(3001);
+
+const grid = new pathfinding.Grid(
+  map.size[0] * map.gridDivision,
+  map.size[1] * map.gridDivision
+);
+
+const finder = new pathfinding.AStarFinder({
+  allowDiagonal: true,
+  dontCrossCorners: true,
+});
+
+const findPath = (start, end) => {
+  const gridClone = grid.clone();
+  const path = finder.findPath(start[0], start[1], end[0], end[1], gridClone);
+  return path;
+};
+
+const updateGrid = () => {
+  map.items.forEach((item) => {
+    if (item.walkable || item.wall) {
+      return;
+    }
+    const width =
+      item.rotation === 1 || item.rotation === 3 ? item.size[1] : item.size[0];
+    const height =
+      item.rotation === 1 || item.rotation === 3 ? item.size[0] : item.size[1];
+    for (let x = 0; x < width; x++) {
+      for (let y = 0; y < height; y++) {
+        grid.setWalkableAt(
+          item.gridPosition[0] + x,
+          item.gridPosition[1] + y,
+          false
+        );
+      }
+    }
+  });
+};
+updateGrid();
+console.log(findPath([3, 3], [4, 7]));
 
 const characters = [];
 
